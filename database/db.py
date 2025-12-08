14f9a2788f4b2c2523ebf0f5ff224ffa4aec230a
karanbannaa108 <bhaigaming524@gmail.com>
2025-12-08 05:33:36 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,11 +1,8 @@
 from pymongo import MongoClient
 import os
 
-# Use environment variable for security
 MONGO_URI = os.environ.get("MONGO_URI")
-if not MONGO_URI:
-    raise Exception("MONGO_URI environment variable not set")
 
 client = MongoClient(MONGO_URI)
-db = client["free_backend"]
+db = client["production"]
 users_col = db["users"]
